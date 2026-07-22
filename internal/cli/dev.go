10878e9e93fcb19f21b453cfa71245b0910a6306
TirthBora <tirthbora8806@gmail.com>
2026-07-22 18:43:49 +0530

    feat(dev): wire project detection, runner, and process manager

--- internal/cli/dev.go
+++ internal/cli/dev.go
@@ -1,16 +1,15 @@
 package cli
 
 import (
-	"fmt"
-
+	"github.com/TirthBora/catalyst/internal/dev"
 	"github.com/spf13/cobra"
 )
 
 var devCmd = &cobra.Command{
 	Use:   "dev",
 	Short: "Start the development server",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Development mode is not implemented yet.")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return dev.Run()
 	},
 }
 
--- internal/dev/dev.go
+++ internal/dev/dev.go
@@ -1,5 +1,20 @@
 package dev
 
-func Run() string {
-	return "starting dev workflow"
+import (
+	"github.com/TirthBora/catalyst/internal/process"
+	"github.com/TirthBora/catalyst/internal/project"
+	"github.com/TirthBora/catalyst/internal/runner"
+)
+
+func Run() error {
+	proj, err := project.Detect()
+	if err != nil {
+		return err
+	}
+	cmd := runner.New(proj)
+	manager := process.New()
+	if err := manager.Start(cmd); err != nil {
+		return err
+	}
+	select {}
 }
